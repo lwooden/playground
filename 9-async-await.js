27452304cae37ee7727ca01b38408a61fa86d1ac
lwooden <lowell.wooden@gmail.com>
2019-05-13 22:40:13 -0400

    async await example

--- 9-async-await.js
+++ 9-async-await.js
@@ -10,15 +10,29 @@ const add = (a, b) => {
     })
 }
 
-const doWork = async () => {
-    const sum = await add(1, -99)
-    const sum2 = await add(sum, 50)
-    const sum3 = await add(sum2, -3)
-    return sum3
+
+// const doWork = async () => { // async function "always" returns a "Promise"
+
+//     throw new Error('something went wrong!')
+//     return "Low" // the Promise has been fulfilled with the string "Low"
+// }
+
+const doWork = async () => { // async function "always" returns a "Promise"
+
+        // we have access to all of these variables within the "same scope"
+        // this is difficult to achieve with callbacks and traditional promise style
+        const sum = await add(1, 99)
+        console.log("Processing request 1")
+        const sum2 = await add(sum, 3)
+        console.log("Processing request 2")
+        const sum3 = await add(sum2, 1)
+        console.log("rocessing request 3")
+        return sum3
+
 }
 
-doWork().then((result) => {
-    console.log('result', result)
-}).catch((e) => {
-    console.log('e', e)
-})
+// doWork().then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
